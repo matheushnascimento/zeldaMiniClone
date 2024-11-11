7f0b860aa4f64bdc772c0868ef838cb1c3c4cb13
matheushnascimento <matheus.mhddn@gmail.com>
2024-11-11 15:46:22 -0300

    feat: render blank window

---
+++ src/Game.java
@@ -0,0 +1,29 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+    public static int WIDTH = 480, HEIGHT = 480;
+
+    public Game() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        JFrame frame = new JFrame();
+
+        frame.add(game);
+        frame.setTitle("Mini Zelda"); // Seta título da janela
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Reset da posição da janela
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// finalizar processo Java quando JFrame for finalizado
+        frame.setVisible(true);
+
+    }
+}
